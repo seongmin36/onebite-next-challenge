4f55d9b8d11529ab071448d44211c81e6db895be
seongmin36 <whtjdals3625@gmail.com>
2025-10-04 03:36:12 +0900

    section02 SSR 직렬연결 단점

--- section02/src/lib/fetch-random-books.ts
+++ section02/src/lib/fetch-random-books.ts
@@ -1,15 +1,13 @@
 import { Book } from "@/types/book";
 
-export default async function fetchBooks(): Promise<Book[]> {
-  const url = `http://localhost:12345/book`;
+export default async function fetchRandomBooks(): Promise<Book[]> {
+  const url = `http://localhost:12345/book/random`;
 
   try {
     const response = await fetch(url);
-
     if (!response.ok) {
       throw new Error();
     }
-
     return await response.json();
   } catch (err) {
     console.error(err);
--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,48 +2,45 @@
 // unique한 className으로 작성
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import books from "@/mock/books.json";
 import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { useEffect } from "react";
 import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
-
-  // 서버에서만 실행되는 코드이기 때문에 브라우저에서 출력 X
-  console.log("서버사이드 프롭스에요");
-
-  const data = "hello";
+// 서버사이드 렌더링 (getServerSideProps() : Next.js에서 약속된 SSR 함수)
+export const getServerSideProps = async () => {
+  // SSR 직렬 연결 1개 실행 후 나머지 실행(단점)
+  const allBooks = await fetchBooks();
+  const recoBooks = await fetchRandomBooks();
 
   return {
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
 
 // Home 컴포넌트도 서버에서 한 번 실행되기 때문에 (사전 렌더링) window.loaction 같은 브라우저 메서드를 사용할 수 없다.
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data);
-
-  useEffect(() => {
-    console.log(window);
-  });
+  console.log(allBooks);
+  console.log(recoBooks);
 
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
