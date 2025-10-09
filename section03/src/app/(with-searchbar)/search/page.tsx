6a6e15a69fda77c344988068c76f2e1d3a9131f2
seongmin36 <whtjdals3625@gmail.com>
2025-10-10 04:36:41 +0900

    section03 App Router finished

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,17 +1,18 @@
-import ClientComponent from "@/components/client-component";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ q: string }>;
+  searchParams: Promise<{ q?: string }>;
 }) {
   const { q } = await searchParams;
+
   return (
     <div>
-      Search 페이지 {q}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,24 +1,41 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { ChangeEvent } from "react";
-import { useState } from "react";
 
-// static한 페이지는 js와 rsc 모두 pre fetching하지만, dynamic한 페이지는 rsc만 불러온다.
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./serachbar.module.css";
+
 export default function Searchbar() {
-  const [search, setSearch] = useState("");
   const router = useRouter();
+  // useSearchParams : 현재 페이지에 적용된 query string의 값을 꺼내오는 역할
+  // PageRouter에서 썼던 useRouter와 다르기 때문에 router.query.q로 꺼내오지 못함.
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState("");
 
-  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+  // "q"라는 query string을 Get으로 가져옴
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   const onSubmit = () => {
+    if (!search || q === search) return;
     router.push(`/search?q=${search}`);
   };
 
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
-    <div>
-      <input value={search} onChange={onChangeSearch} type="text" />
+    <div className={style.container}>
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
       <button onClick={onSubmit}>검색</button>
     </div>
   );
