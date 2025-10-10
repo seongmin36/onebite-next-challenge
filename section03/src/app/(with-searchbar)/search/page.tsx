3b065d88a54933ee570a6d2798bf38367bf42cb4
seongmin36 <whtjdals3625@gmail.com>
2025-10-11 04:27:05 +0900

    section03 Finished

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 
+// useCallback(), memoization과 같은 처리에서 비동기 X (client component)
 export default async function Page({
   searchParams,
 }: {
