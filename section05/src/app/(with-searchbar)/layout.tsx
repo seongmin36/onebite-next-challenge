f7d867b406fc6d5fe2503791587aec9ce84ba5c6
seongmin36 <whtjdals3625@gmail.com>
2025-10-11 17:09:28 +0900

    section05 클라이언트 라우터 캐시 finished

--- section05/src/app/(with-searchbar)/layout.tsx
+++ section05/src/app/(with-searchbar)/layout.tsx
@@ -5,6 +5,8 @@ import { Suspense } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* 클라이언트 라우트 캐시 확인용 DateString */}
+      <div>{new Date().toLocaleString()}</div>
       {/* 넥스트 서버 측에서 사전 렌더링을 진행할 때 Suspense로 묶여있는 컴포넌트들은 미완성 상태로 남겨짐 */}
       {/* -> 곧바로 렌더링 X -> fallback으로 대체 UI 생성 (비동기작업이 종료될 때까지)*/}
       <Suspense fallback={<div>Loading...</div>}>
