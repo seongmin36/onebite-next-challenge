2fa089154881d41f8999b0f446f14ad9fe7850de
seongmin36 <whtjdals3625@gmail.com>
2025-10-18 02:50:25 +0900

    sction07: 서버 액션 - 클라이언트 컴포넌트에서의 서버 액션 finished

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,9 +1,10 @@
 "use server";
 
+import delay from "@/utils/delay";
 import { revalidatePath, revalidateTag } from "next/cache";
 
 // 서버 액션으로 간단하게 API를 대체할 수 있는 코드 : 서버측에서만 실행, 브라우저에는 전달만 -> 보안성
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
@@ -11,9 +12,13 @@ export async function createReviewAction(formData: FormData) {
   console.log(bookId, content, author);
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입려해주세요",
+    };
   }
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -22,6 +27,11 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     console.log(response.status);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
     // 서버에서 넥스트 페이지에 bookId의 경로를 다시 생성할 것을 요청 (서버측에서) -> 재검증 로직
     // 1. 서버측에서만 호출할 수 있는 메서드이다.
     // 2. 이 페이지(/book/[id])의 모든 캐시를 전부 재검증하여 데이터 캐시마저도 무효화한다. (cache: "force-cache" 무효화)
@@ -41,8 +51,15 @@ export async function createReviewAction(formData: FormData) {
 
     // 5) 태그 기준, 데이터 캐시 재검증 (/book/[id]/page.tsxt에서 태그 설정)
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (e) {
     console.error(e);
-    return;
+    return {
+      status: false,
+      error: `리뷰 wjwkd 실패했습니다: ${e}`,
+    };
   }
 }
